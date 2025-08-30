61ade8633b70ae230162740084397834f7550d87
kubsab <squaremail45@gmail.com>
2025-08-30 12:02:45 +0100

    Added automatic geocodeing to find (long,lat) of cities and calculate distances

---
+++ main.py
@@ -0,0 +1,6 @@
+from res.distances import distances
+
+current_city = "london"
+target_city = "minsk"
+
+print(distances[f"{current_city}>{target_city}"])
--- res/distances.py
+++ res/distances.py
@@ -1,15 +1,57 @@
 from geopy import distance
-cities = {
-    "london": (51.510311052239075, -0.12822038593055046),
-    "minsk": (53.924508796618085, 27.60176254136057)
-}
+from geopy.geocoders import get_geocoder_for_service
+import json
+
+query_cities = ["london", "minsk", "moscow", "warsaw"]
+cities_cache = {}
+fr = open("distances_cache.JSON", "r")
+json_parsed = json.loads(fr.read())
+print(json_parsed)
+
+
+def check_cache():
+    counter = 0
+    already_cached_index = []
+
+    for city in query_cities:
+        if city in json_parsed:
+            already_cached_index.append(query_cities.index(city)-counter)
+            counter += 1
+            print(f"{city} present")
+    return already_cached_index
+
+
+already_cached_index = check_cache()
+for index in already_cached_index:
+    del query_cities[index]
+
+
+def geocode(geocoder, config, query):
+    cls = get_geocoder_for_service(geocoder)
+    geolocator = cls(**config)
+    location = geolocator.geocode(query)
+    return (location.latitude, location.longitude)
+
+
+for city in query_cities:
+    cities_cache.update(
+            {f"{city}": geocode("nominatim",
+                                dict(user_agent="Mozilla/5.0 (X11; Linux x86_64; rv:142.0) Gecko/20100101 Firefox/142.0"),
+                                city)
+             }
+            )
+
+json_parsed.update(cities_cache)
+print(json_parsed)
+# If there is nothing in distances_cache.JSON initialize
+fw = open("distances_cache.JSON", "w")
+fw.write(json.dumps(json_parsed))
 
 distances = {}
 
-for city in cities.keys():
-    for target in cities.keys():
-        distances.update({f"{city}>{target}": round(distance.distance(cities[city], cities[target]).miles, 1)})
+for city in json_parsed.keys():
+    for target in json_parsed.keys():
+        if city != target:
+            distances.update({f"{city}>{target}": round(distance.distance(json_parsed[city], json_parsed[target]).miles, 1)})
 
-
-if __name__ == "__main__":
-    print(distances)
+print(distances)
